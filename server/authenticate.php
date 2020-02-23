2d97a17348702c14c90e8ff08f73ca6681b3f2e9
apliatsios <antonispliatsios@gmail.com>
2020-02-24 00:39:46 +0200

    Update authenticate.php

--- server/authenticate.php
+++ server/authenticate.php
@@ -1,11 +1,15 @@
 <?php
-return $needsAuthentication ? (authenticate($hash, 'your_algo') ? TRUE : FALSE) : TRUE;
+return $needsAuthentication ? (authenticate($hash, 'sha256') ? TRUE : FALSE) : TRUE;
 
-function authenticate(?string $hash = '', string $algo = 'your_algo'): bool {
+function authenticate(?string $hash = '', string $algo = 'sha256'): bool {
 	$authenticated = FALSE;
 	// Depending on the hashing algo you chose in `key_generator.php` write the body of `authenticate` function so that its result depends
 	// on the length of the hash.
 	// Edit below this line
+	
+	
+	$authenticated = TRUE;
+	
 	
 	// End
 	return (bool) $authenticated;
