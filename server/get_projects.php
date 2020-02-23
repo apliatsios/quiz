611035b616eafc912be264b462553f2ffcf5fd05
apliatsios <antonispliatsios@gmail.com>
2020-02-24 00:38:06 +0200

    Update get_projects.php

--- server/get_projects.php
+++ server/get_projects.php
@@ -1,2 +1,44 @@
-<?php 
+<?php
 // see sample-data.txt on what to return
+
+$projects = ' [
+	{
+		"name": "Wordpress",
+		"user": 1
+	},
+	{
+		"name": "Blog",
+		"user": 1
+	},
+	{
+		"name": "Wiki",
+		"user": 2
+	},
+	{
+		"name": "Magento",
+		"user": 3
+	},
+	{
+		"name": "Codeigniter",
+		"user": 1
+	},
+	{
+		"name": "Slim",
+		"user": 3
+	},
+	{
+		"name": "Laravel",
+		"user": 2
+	},
+	{
+		"name": "Drupal",
+		"user": 1
+	},
+	{
+		"name": "Joomla",
+		"user": 4
+	}
+]';
+
+return json_decode($projects);
+?>
