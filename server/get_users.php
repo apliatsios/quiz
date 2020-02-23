1c95023e65b30a52f4a3504add8ab57af8e94466
apliatsios <antonispliatsios@gmail.com>
2020-02-24 00:36:45 +0200

    Update get_users.php

--- server/get_users.php
+++ server/get_users.php
@@ -1,2 +1,25 @@
 <?php
 // see sample-data.txt on what to return
+
+
+
+$users = '[
+	{
+		"id": 1,
+		"name": "John",
+		"lastName": "Doe"
+	},
+	{
+		"id": 2,
+		"name": "Chris",
+		"lastName": "Black"
+	},
+	{
+		"id": 3,
+		"name": "George",
+		"lastName": "Best"
+	}
+]';
+
+return json_decode($users);
+?>
